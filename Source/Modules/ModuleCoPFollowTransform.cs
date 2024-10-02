2e375a9f64642ef446e0cc6ed96d03b45b571f77
Sofie <63377159+SofieBrink@users.noreply.github.com>
2024-10-02 20:30:32 +0200

    Add ModuleCoPFollowTransform
    - Temporarily added ModuleCoPFollowTransform to BCS for the main parachutes.

---
+++ Source/Modules/ModuleCoPFollowTransform.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+namespace BoringCrewServices.Modules
+{
+    public class ModuleCoPFollowTransform : PartModule
+    {
+        public const string MODULENAME = nameof(ModuleCoPFollowTransform);
+        [KSPField]
+        public string transformName;
+
+        private Transform followTransform;
+
+        public override void OnLoad(ConfigNode node)
+        {
+            base.OnLoad(node);
+            if (HighLogic.LoadedScene != GameScenes.LOADING)
+            {
+                if (transformName != null) followTransform = part.FindModelTransform(transformName);
+                if (followTransform == null) Debug.LogError($"[{MODULENAME}] transformName was empty or does not exist.");
+            }
+        }
+
+        public void FixedUpdate()
+        {
+            if (followTransform != null) part.CoPOffset = part.transform.InverseTransformPoint(followTransform.position);
+        }
+    }
+}
